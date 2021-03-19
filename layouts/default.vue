3dbd845ac454fb15900620b706bda084d7b670dc
iolinpic <iolinpic@gmail.com>
2021-03-19 16:55:53 +0300

    update styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="bg-gray-800 font-sans leading-normal tracking-normal mt-12">
+  <div class="font-serif text-base">
     <topbar></topbar>
-    <div class="flex flex-col md:flex-row">
+    <div >
       <sidebar></sidebar>
-      <main class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+      <main >
         <Nuxt/>
       </main>
     </div>
