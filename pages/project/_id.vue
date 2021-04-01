0229710ba1c612bdc172af1e91758e6fc6fea86f
iolinpic <iolinpic@gmail.com>
2021-04-01 03:17:04 +0300

    start project dev

---
+++ pages/project/_id.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+<script>
+export default {
+  name:"ProjectComponent",
+  async asyncData(context) {
+    return { projectId: context.params.id }
+  },
+  data() {
+    return {
+      project: undefined,
+    }
+  },
+  async fetch(){
+    let res = await this.$axios.get(`/project/${this.projectId}`);
+    this.project = res.data;
+  },
+}
+</script>
